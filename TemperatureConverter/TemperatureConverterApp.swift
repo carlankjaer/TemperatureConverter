efd562af031d09b566e46038c04bb28a0a3ea7f7
Caroline <carlankjaer@gmail.com>
2023-12-20 14:26:00 +0100

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Caroline Ankjær Andersen on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
